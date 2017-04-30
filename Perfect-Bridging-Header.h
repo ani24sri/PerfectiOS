3bdcdf2782b6e92dd7720c404e0af03ba87042e0
{Ascence}_//Of_= ['Innovation'] <anirudh@Anirudhs-iMac.local>
2017-04-30 15:55:06 +0530

    New changes

---
+++ Perfect-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Perfect-Bridging-Header.h
+//  Perfect
+//
+//  Created by Anirudh on 19/03/17.
+//  Copyright © 2017 Electro Cheval. All rights reserved.
+//
+#import "iCarousel.h"
--- ViewController.swift
+++ ViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , iCarouselDelegate , iCarouselDataSource  {
 
+    var imgArray :NSMutableArray = NSMutableArray()
+
+    @IBOutlet weak var DisplayView : iCarousel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgArray = ["logo.png","s1.png","s3.png","s5.png","s6.jpg"]
+        DisplayView.type = iCarouselType.wheel
+        DisplayView.contentMode = .scaleAspectFit
+        DisplayView.reloadData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        var imgVw : UIImageView!
+        
+        if view == nil
+        {
+            imgVw = UIImageView(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+            imgVw.contentMode = .scaleAspectFit
+        }
+        else {
+            imgVw = view as! UIImageView
+        }
+        imgVw.image = UIImage(named: "\(imgArray.object(at: index))" )
+        return imgVw
+    }
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return imgArray.count
+    }
+    
 }
-
